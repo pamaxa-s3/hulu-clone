0f061ef1ba4297918d2ff1b53cb7f6264c3d0057
Strynzha Roman <johndoe@example.com>
2020-08-27 16:42:11 +0300

    added Nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './Header/Header';
+import Nav from './Nav/Nav';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Nav />
       {/* Nav */}
       {/* Result */}
     </div>
