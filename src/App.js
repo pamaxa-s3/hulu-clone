8c2c0c58934b8b69328dbaeb709958cceb42ee77
Strynzha Roman <johndoe@example.com>
2020-08-28 15:49:42 +0300

    added videaCards

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import './App.css';
 import Header from './Header/Header';
 import Nav from './Nav/Nav';
+import Results from './Results/Results';
+import { useState } from 'react';
+import requests from './utilites/requests';
 
 function App() {
+
+  const [selectedOption, setSelecderOption] = useState(requests.fetchTrending);
+
   return (
     <div className="App">
       <Header />
-      <Nav />
-      {/* Nav */}
-      {/* Result */}
+      <Nav setSelecderOption={setSelecderOption} />
+      <Results selectedOption={selectedOption}/>
     </div>
   );
 }
